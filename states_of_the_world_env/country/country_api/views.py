ca7cce30d9a34340ba60cb174d84eea765c5e326
AndriciAndra <79244898+AndriciAndra@users.noreply.github.com>
2022-12-10 22:31:05 +0200

    add exceptions

--- states_of_the_world_env/country/country_api/views.py
+++ states_of_the_world_env/country/country_api/views.py
@@ -1,6 +1,8 @@
+import django.core.exceptions
+
 from .models import Country
 from django.core import serializers
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotFound
 import json
 
 
@@ -14,7 +16,10 @@ def countries_all_view(request):
 
 def countries_top_view(request, my_top):
     if request.method == "GET":
-        countries = Country.objects.all().order_by('-' + my_top)[:10]
+        try:
+            countries = Country.objects.all().order_by('-' + my_top)[:10]
+        except django.core.exceptions.FieldError:
+            return HttpResponseNotFound('<h1>Not found</h1>')
         serializer = serializers.serialize("json", countries, fields=('id', 'name', my_top))
         return JsonResponse(json.loads(serializer), safe=False, json_dumps_params={'indent': 2})
 
@@ -22,6 +27,8 @@ def countries_top_view(request, my_top):
 def countries_time_view(request, my_time):
     if request.method == "GET":
         countries = Country.objects.filter(time_zone=my_time)
+        if not countries:
+            return HttpResponseNotFound('<h1>Not found</h1>')
         serializer = serializers.serialize("json", countries, fields=('id', 'name', 'time_zone'))
         return JsonResponse(json.loads(serializer), safe=False, json_dumps_params={'indent': 2})
 
@@ -31,10 +38,13 @@ def countries_language_view(request, my_language):
         countries = Country.objects.all()
         my_list = list()
         for country in countries:
-            if my_language in country.languages:
+            languages_lower = [language.lower() for language in country.languages.split(" ")]
+            if my_language.lower() in languages_lower:
                 my_list.append(country.id)
         countries = Country.objects.filter(pk__in=my_list)
-        serializer = serializers.serialize("json", countries)
+        if not countries:
+            return HttpResponseNotFound('<h1>Not found</h1>')
+        serializer = serializers.serialize("json", countries, fields=('id', 'name', 'languages'))
         return JsonResponse(json.loads(serializer), safe=False, json_dumps_params={'indent': 2})
 
 
@@ -43,8 +53,11 @@ def countries_government_view(request, my_government):
         countries = Country.objects.all()
         my_list = list()
         for country in countries:
-            if my_government in country.government:
+            government_lower = [government.lower() for government in country.government.split(" ")]
+            if my_government in government_lower:
                 my_list.append(country.id)
         countries = Country.objects.filter(pk__in=my_list)
-        serializer = serializers.serialize("json", countries)
+        if not countries:
+            return HttpResponseNotFound('<h1>Not found</h1>')
+        serializer = serializers.serialize("json", countries, fields=('id', 'name', 'government'))
         return JsonResponse(json.loads(serializer), safe=False, json_dumps_params={'indent': 2})
