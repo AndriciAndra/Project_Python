291dea315ce330bbd59b2318a6991e7e60309a6c
AndriciAndra <79244898+AndriciAndra@users.noreply.github.com>
2022-12-10 22:25:26 +0200

    create client that calls APIs

---
+++ states_of_the_world_env/Client.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+
+def print_response(my_url, parameter):
+    response = requests.get("http://127.0.0.1:8000/countries" + my_url)
+    json_response = response.json()
+    for country in json_response:
+        data = {
+            "id": country["pk"],
+            "name": country["fields"]["name"],
+            parameter: country["fields"][parameter]
+        }
+        print(json.dumps(data, indent=3))
+
+
+def run_api():
+    while True:
+        client_request = input()
+        if client_request == "top 10 tari dupa densitate":
+            print_response("/top-10-countries-density_per_km2", "density_per_km2")
+        if client_request == "top 10 tari dupa population":
+            print_response("/top-10-countries-population", "population")
+        if client_request == "lista tari cu limba engleza":
+            print_response("/language-english", "languages")
+        if client_request == "lista tari cu UTC+1":
+            print_response("/time-zone/UTC+1", "time_zone")
+        if client_request == "lista tari monarhice":
+            print_response("/government-monarchy", "government")
+        if client_request == "lista tarilor din lume":
+            response = requests.get("http://127.0.0.1:8000/countries/")
+            data = response.json()
+            print(json.dumps(data, indent=4))
+        if client_request == 'exit':
+            return
+
+
+if __name__ == "__main__":
+    run_api()
